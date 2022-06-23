179107a6af98c2bb4488d8b9acfdd05f07efcd03
KUMARAN T E <94185252+KUMARAN1601@users.noreply.github.com>
2022-06-23 19:16:24 +0530

    Create personalloans.php

---
+++ personalloans.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="//cdjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="css" href="Loans.css">
+  <title>Loans</title>
+</head>
+<body>
+  <div><h1 class="text-center" style="font-weight: 500; color: black;">Personal Loans</h1></div>
+  <div class="container" id="Card">
+  <div class="card mb-3">
+    <img class="card-img-top" src="wedding.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h3 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Wedding Loan</h3>
+      <hp class="card-text">Weddings are an extremely important event in India. They come with a long list of expenses. All of us dream for this day (for ourselves as well as for our children) to be very memorable, and there is nothing we would not do to fulfill this dream. Thus, many lenders have started offering personal loans for wedding, which help us make our special day memorable. Loans can be taken by the would-be brides, grooms or any of the family members. In this type of personal loan, the loan amount can be customized as per the individual's needs.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+      <p>Loan amount from as low as Rs.1,000 up to Rs.25 lakh.</p>
+      <p>Loan repayment tenure ranging between 3 months and 72 months.</p>
+      <p>Minimal documentation process.</p>
+      <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="rennovationimg.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Home Renovation Loan</h5>
+      <p class="card-text">Taking home loans is very common for buying real estate but we rarely consider what happens when we have to renovate our houses. We either postpone it because it is an expensive exercise, or end up compromising on several aspects in order to manage costs. Thus identifying this need, home improvement loans are offered, which help us take care of repairing costs, buying new material, labourer’s charges, etc. This type of personal loan which helps us renovate our house, gives the added advantage of boosting the total economic value of the house in the real estate market.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+      <p>Loan amount ranging from Rs.2 lakh to Rs.40 lakh</p>
+      <p>Loan repayment tenure of up to 30 years</p>
+      <p> Rate of interest starting from as low as 6.85% p.a.</p>
+      <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="travelloan.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Travel Loan</h5>
+      <p class="card-text">This is designed especially for people who love holidaying. They are called travel/vacation/holiday loans, wherein one can go around the world, however, pay the bills later with holiday loans. In this type of personal loan, there's often an added benefit of travel insurance, in order to give foolproof protection.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+      <p>Simple and quick documentation process.</p>
+      <p>Loan amount of as much as Rs.40 lakh</p>
+      <p>Flexible loan repayment period which can be extended up to as much as 72 months.</p>
+      <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="medicalloan.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title" style="color: rgb(0, 0, 0); font-size: 25ppx">Medical Loan</h5>
+      <p class="card-text">Medical emergencies can be extremely frightening, especially if you are unprepared! While many people have started investing in medical insurance, a large number of people are still uncovered, especially for emergencies. And at times, even insurance will not cover certain medical expenses.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+      <p>Must be a full-time salaried employee with a minimum take-home salary of ₹ 30,000/month</p>
+      <p>Must be a self-employed professional with an income of at least ₹ 30,000/month.</p>
+      <p>Must be above 23 years and below 55 years of age</p>
+      <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="debt.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Debt-consolidation Loan</h5>
+      <p class="card-text">You can take a Personal Loan of up to Rs 40 lakh to consolidate your debt obligations and streamline payments, while also improving loan terms. If you have had a very good credit history, banks may even offer lower, more competitive interest rates on the Personal Loan, thereby helping you bring down your overall interest rate.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+      <p>Must be between 21 years and 60 years of age</p>
+      <p>Must have had a job for at least 2 years, with a minimum of 1 year with the current employer</p>
+      <p>For self-employed: Applicants must have a specified minimum income (may vary with location). Financial documents to show stability of income may also be required by the bank.</p>
+      <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="education.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Education Loan</h5>
+      <p class="card-text">There are a number of lenders who offer personal loans which can be used for educational purpose. Right from paying the tuition fees of your child to any other academic requirement, you can use the education loan to meet the expenses.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+       <p>Loan repayment tenure up to 15 years.</p>
+       <p>Rate of interest starts at as low as 6.35% p.a.</p>
+       <p> Loan amount of up to Rs.20 lakh.</p>
+       <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="pension.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h3 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Pension Loan</h3>
+      <p class="card-text">Pension loans are exclusively for retired personals, and hence, the usual eligibility criteria do not apply in this special scheme. Under this scheme, some banks provide 7 to 10 times the amount of pension the pensioner drew the month previous to submitting the loan application.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+        <p>Loan amount that starts at Rs.25,000 and goes up to Rs.14 lakh</p>
+        <p>Loan repayment tenure of up to 60 months</p>
+        <p>Discounted rate of interest of as low as 11.70% p.a.</p>
+        <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
+  
+  
+</body>
+<script href="" type="js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> 
+</html>
