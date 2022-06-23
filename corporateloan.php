36a1425e4a1bd904244c73c535a05579e10aede0
KUMARAN T E <94185252+KUMARAN1601@users.noreply.github.com>
2022-06-23 19:14:02 +0530

    Create corporateloan.php

---
+++ corporateloan.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="//cdjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <title>Loans</title>
+</head>
+<body>
+<div><h1 class="text-center" style="font-weight: 500; color: black;">Corporate / Project Loans</h1></div>
+  <div class="container" id="Card">
+    <div class="card mb-3">
+      <img class="card-img-top" src="project.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Builder / Deveoplers</h5>
+        <p class="card-text">Building a home for many, is a long term goal and is an expensive affair. In such instances a personal loan won’t cut it. Home construction loans as such have become a specialized product. Higher loan amounts, longer repayment tenures and lower interest rates are hallmark features of these loans. Construction loans are provided both for individual consumers looking to build their dream home as well as to builders looking to construct projects for sale. Below are examples of both types of construction loans and their features.</p>
+        <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+        <p>This loan offers financial solutions for both individual consumers as well as builders. </p>
+        <p>Based on the type of loan availed, the repayment tenure varies. </p>
+        <p>For loans availed under telescopic repayment option.</p>
+        <p>The maximum loan tenure can go up to 30 years whereas other home loan options provide a maximum loan tenure of 20 years.</p>
+        <p>Based on the loan amount availed, the maximum funding of the project varies. </p>
+        <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+      </div>
+    </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="corporate.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h3 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Corporate</h3>
+      <h class="card-text">Corporate loans are sanctioned for an existing business and industrial house which has been in action for at least 5 years and has made profits for at least the last 2 years. In addition to that, the organisation will also be required to have a good credit rating and should have a good track record in terms of being a successful business. In case the organisation had loans in the past, the records in regard to the repayment of the credit are also taken into consideration.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+      <p>Fast sanction of loans.</p>
+      <p>Completely transparent process.</p>
+      <p>Security not required.</p>
+      <p>Competitive pricing.</p>
+      <p>Dropline Overdraft facility of up to Rs.5 lakh to Rs.15 lakh.</p>
+      <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
