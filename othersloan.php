2eba3b6c7a78e28ba7b3d5efa62619cd809fb7b0
KUMARAN T E <94185252+KUMARAN1601@users.noreply.github.com>
2022-06-23 19:15:50 +0530

    Create othersloan.php

---
+++ othersloan.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="//cdjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <title>other loans</title>
+</head>
+<body>
+<div><h1 class="text-center" style="font-weight: 500; color: black;">Other Loans</h1></div>
+  <div class="container" id="Card">
+    <div class="card mb-3">
+      <img class="card-img-top" src="lap.jpeg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Loan Against Property (LAP) - For Individuals</h5>
+        <p class="card-text">Loan for your business or personal needs</p>
+        <p class="card-text">Property that helps you to expand your business needs or meet your personal goals like children’s marriage, education, foreign travel</p>
+        <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+        <p>Children´s Education / Marriage</p>
+        <p>Purchase of another Property</p>
+        <p>Business Expansion</p>
+        <p>Minimum Loan Rs. 2,00,000/-</p>
+        <p>Maximum 15 Years</p>
+        <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+      </div>
+    </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="las.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h3 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Loans against securities</h3>
+      <p class="card-text">Loans against Fixed Deposit of Nationalized Banks, Life Insurance Policies having surrender value and Post office instruments like National Savings.</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+      <p>Loan up to 95% of face value of liquid security or surrender value</p>
+      <p>Minimum Loan Rs.50,000/-</p>
+      <p>Maximum Term up to 20 Years</p>
+      <p>This loan can be clubbed along with Home Loans</p>
+      <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
+  <div class="card mb-3">
+    <img class="card-img-top" src="ltp.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h3 class="card-title" style="color: rgb(0, 0, 0); font-size: 25px;">Loans to professional</h3>
+      <p class="card-text">Loans to professionals for buying their office space and equipment.</p>
+      <p class="card-text">Term up to 15 years | Loan starting from Rs. 2 Lakhs</p>
+      <h4 class="card-text" style="color: rgb(0, 0, 0); font-size: 25px;">Key highlights :</h4>
+      <p>Fast sanction of loans.</p>
+      <p>Completely transparent process.</p>
+      <p>Security not required.</p>
+      <p>Competitive pricing.</p>
+      <p>Dropline Overdraft facility of up to Rs.5 lakh to Rs.15 lakh.</p>
+      <h3>Click here:  <a href="profiledisplay.php"  style="color: #052f89; font-size: 20px; font-weight: 300px;">Apply now</a>
+    </div>
+  </div>
